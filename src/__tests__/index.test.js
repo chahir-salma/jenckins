9754a109a46563df38e5deb092ad39ea9f9f91b9
Houssam02 <houssammandour2@gmail.com>
2025-01-21 20:14:59 +0100

    upgrade the couvrage

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Ensure you're importing from the correct path
+import ReactDOM from 'react-dom/client'; // Import ReactDOM from 'react-dom/client'
 import App from '../App.jsx'; // Adjust the import path as necessary
 
+// Mocking ReactDOM.createRoot
 jest.mock('react-dom/client', () => ({
     createRoot: jest.fn(() => ({
-        render: jest.fn(),
+        render: jest.fn(), // Mocking the render function
     })),
 }));
 
 beforeEach(() => {
+    // Create a root element and append it to the document body
     const root = document.createElement('div');
     root.id = 'root';
     document.body.appendChild(root);
@@ -16,6 +18,14 @@ beforeEach(() => {
 
 test('renders with App and root div', () => {
     require('../index'); // This should call createRoot and render App
+    
+    // Check if createRoot was called with the correct argument
     expect(ReactDOM.createRoot).toHaveBeenCalledWith(document.getElementById('root'));
-    expect(ReactDOM.createRoot().render).toHaveBeenCalledWith(<App />);
+    
+    // Check if render was called with <App />
+    expect(ReactDOM.createRoot().render).toHaveBeenCalledWith(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 });
