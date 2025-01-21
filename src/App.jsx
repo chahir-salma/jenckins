5f5b935bafd834ad77a4ff288a6dd236b0fe4e60
Houssam02 <houssammandour2@gmail.com>
2025-01-21 23:57:02 +0100

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="background">
-      <h1 className="title">Pipeline - It works with coverage</h1>
+      <h1 className="title">Todo List Application By ReactJs</h1>
       <div className="App">
         <h2>To Do List</h2>
         <p>Total Items: {todos.length}</p> {/* Updated for clarity */}
